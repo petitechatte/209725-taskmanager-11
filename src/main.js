d891729594fd94a975cbb735d006224f9c05198d
petitechatte <natalyavaskot@gmail.com>
2020-04-03 03:06:40 +0300

    Добавлена отрисовка трех карточек заданий

--- src/main.js
+++ src/main.js
@@ -323,19 +323,78 @@ const createTaskEditingForm = () => {
   )
 };
 
+const createTaskTemplate = () => {
+  return (
+    `<article class="card card--black">
+      <div class="card__form">
+        <div class="card__inner">
+          <div class="card__control">
+            <button type="button" class="card__btn card__btn--edit">
+              edit
+            </button>
+            <button type="button" class="card__btn card__btn--archive">
+              archive
+            </button>
+            <button
+              type="button"
+              class="card__btn card__btn--favorites card__btn--disabled"
+            >
+              favorites
+            </button>
+          </div>
+
+          <div class="card__color-bar">
+            <svg class="card__color-bar-wave" width="100%" height="10">
+              <use xlink:href="#wave"></use>
+            </svg>
+          </div>
+
+          <div class="card__textarea-wrap">
+            <p class="card__text">Example default task with default color.</p>
+          </div>
+
+          <div class="card__settings">
+            <div class="card__details">
+              <div class="card__dates">
+                <div class="card__date-deadline">
+                  <p class="card__input-deadline-wrap">
+                    <span class="card__date">23 September</span>
+                    <span class="card__time">16:15</span>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </article>`
+  );
+};
+
 const renderTemplate = (container, template, place = `beforeend`) => {
   container.insertAdjacentHTML(place, template);
 };
 
+// рендеринг шапки приложения и контейнера для контента
+
 renderTemplate(siteMainControlElement, createMenu());
 renderTemplate(siteMainElement, createFilter());
 renderTemplate(siteMainElement, createBoard());
+
+// рендеринг блока сортировки и контейнера для карточек заданий
 
 const board = siteMainElement.querySelector(`.board`);
 
 renderTemplate(board, createSortingControls());
 renderTemplate(board, createTasksContainer());
 
+// рендеринг карточек заданий и формы редактирования задания
+
 const tasksContainer = board.querySelector(`.board__tasks`);
 
 renderTemplate(tasksContainer, createTaskEditingForm());
+
+for (let i = 0; i < 3; i++) {
+  renderTemplate(tasksContainer, createTaskTemplate());
+}
+
