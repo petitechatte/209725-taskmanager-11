2c012f6cf25ff3ccab800c5cb4233fa89947288a
petitechatte <natalyavaskot@gmail.com>
2020-04-05 01:55:03 +0300

    Установлен и настроен Webpack

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const path = require(`path`);
+
+module.exports = {
+  mode: `development`,
+  entry: `./src/main.js`,
+  output: {
+    filename: `bundle.js`,
+    path: path.join(__dirname, `public`)
+  },
+  devtool: `source-map`
+};
