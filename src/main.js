e37bbc9fced8b1150ca08e95e74f6d34f5653b74
petitechatte <natalyavaskot@gmail.com>
2020-04-05 18:48:26 +0300

    Создан модуль для компонента главного меню

--- src/main.js
+++ src/main.js
@@ -1,42 +1,9 @@
-"use strict";
+import {createMenu} from "./components/menu.js";
 
 const TASKS_NUMBER = 3;
 
 const siteMainElement = document.querySelector(`.main`);
 const siteMainControlElement = siteMainElement.querySelector(`.main__control`);
-
-const createMenu = () => {
-  return (
-    `<section class="control__btn-wrap">
-      <input
-        type="radio"
-        name="control"
-        id="control__new-task"
-        class="control__input visually-hidden"
-      />
-      <label for="control__new-task" class="control__label control__label--new-task"
-        >+ ADD NEW TASK</label
-      >
-      <input
-        type="radio"
-        name="control"
-        id="control__task"
-        class="control__input visually-hidden"
-        checked
-      />
-      <label for="control__task" class="control__label">TASKS</label>
-      <input
-        type="radio"
-        name="control"
-        id="control__statistic"
-        class="control__input visually-hidden"
-      />
-      <label for="control__statistic" class="control__label"
-        >STATISTICS</label
-      >
-    </section>`
-  );
-};
 
 const createFilter = () => {
   return (
